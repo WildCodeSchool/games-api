7a5177d85415512ae3051dd385f94e7295e5bd57
Andrea Maugars <andrea.maugars@gmail.com>
2019-10-29 12:09:27 +0100

    Add dynamic PORT from env

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const data = require("./api.json");
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/api/v1/", (req, res) => {
     res.send(data);
 });
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 });
