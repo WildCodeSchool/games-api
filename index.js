0e6b1457561079b09b83f68b8a423c91072a2f59
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-11-04 15:53:42 +0100

    allow all cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const data = require("./api.json");
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/api/v1/", (req, res) => {
     res.send(data);
